17c29c4fa40284529704d3722d998213e9e8e1c0
DMPardo <dmpardoj@udistrital.edu.co>
2026-05-01 22:22:33 -0600

    fix: pagina de inicio en componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,19 @@
 import './App.css';
 
 import logoHuella from './assets/huella.png'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import TopBar from './components/TopBar';
 
 // App principal.
 function App() {
   return (
-    <div className="layout">
-
-      {/*Barra de navegacion.*/}
-      <nav className="navbar">
-
-        {/* Hay que modificarlo y poner el logo. */}
-        <div className="navbar-logo">
-          <img src={logoHuella} alt="Logo Pagina" className= "logo-img"/>
-        </div>
-
-        {/* Lista para redirigir a las paginas */}
-        <ul className="navbar-links">
-          <li>Inicio</li>
-          <li>Adoptar</li>
-          <li>Refugios</li>
-        </ul>
-        
-        {/* Para los dos de autenticacion */}
-        <div className="navbar-auth">
-          <button className="btn-registro">Registrarse</button>
-          <button className="btn-login">Iniciar Sesión</button>
-        </div>
-      </nav>
-
-      {/* Contenido de la pagina */}
-      <main className="page">
-        <h1>Página de registro de mascotas</h1>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <TopBar onMenuClick={() => setMenuVisible(true)}/>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
